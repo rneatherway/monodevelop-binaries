fbe749166848c7a903f7591fdca2440a23d24595
Robin Neatherway <robin.neatherway@gmail.com>
2014-05-01 18:17:47 +0100

    correct 4.2.4.35 binaries

--- script.sh
+++ script.sh
@@ -2,7 +2,7 @@
 
 FSB_PROJ_LOCATION=~/dev/rneatherway-fsharpbinding/monodevelop/MonoDevelop.FSharpBinding/MonoDevelop.FSharp.fsproj.orig
 #MD_LIB_LOCATION=/usr/lib/monodevelop
-MD_LIB_LOCATION=~/dev/monodevelop/main/build
+MD_LIB_LOCATION=~/monodevelop/main/build
 
 
 FILES=$(grep INSERT_FSPROJ_MDROOT $FSB_PROJ_LOCATION | grep HintPath | cut -d'>' -f 2 | cut -d'<' -f1 | tr '\\' '/' | cut -d'/' -f2-)
@@ -11,10 +11,6 @@ for FILE in $FILES
 do
     echo mkdir -p `dirname $FILE`
     mkdir -p `dirname $FILE`
-done
-
-for FILE in $FILES
-do
-    echo cp /usr/lib/monodevelop/$FILE ./$FILE
-    cp /usr/lib/monodevelop/$FILE ./$FILE
+    echo cp $MD_LIB_LOCATION/$FILE ./$FILE
+    cp $MD_LIB_LOCATION/$FILE ./$FILE
 done
