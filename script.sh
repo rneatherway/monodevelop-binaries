8bb45ec2c3c34a66020c246a747203097940b5bc
Robin Neatherway <robin.neatherway@gmail.com>
2014-07-03 22:53:54 +0100

    Add 5.1.1 XS binaries

--- script.sh
+++ script.sh
@@ -2,7 +2,7 @@
 
 FSB_PROJ_LOCATION=~/dev/rneatherway-fsharpbinding/monodevelop/MonoDevelop.FSharpBinding/MonoDevelop.FSharp.fsproj.orig
 #MD_LIB_LOCATION=/usr/lib/monodevelop
-MD_LIB_LOCATION=~/monodevelop/main/build
+MD_LIB_LOCATION=/Applications/Xamarin\ Studio.app/Contents/MacOS/lib/monodevelop/
 
 
 FILES=$(grep INSERT_FSPROJ_MDROOT $FSB_PROJ_LOCATION | grep HintPath | cut -d'>' -f 2 | cut -d'<' -f1 | tr '\\' '/' | cut -d'/' -f2-)
@@ -12,5 +12,5 @@ do
     echo mkdir -p `dirname $FILE`
     mkdir -p `dirname $FILE`
     echo cp $MD_LIB_LOCATION/$FILE ./$FILE
-    cp $MD_LIB_LOCATION/$FILE ./$FILE
+    cp "$MD_LIB_LOCATION/$FILE" ./$FILE
 done
